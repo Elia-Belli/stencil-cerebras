d52d91b951c0b91b9a678cb9c4f019f33146544f
Elia-Belli <elia.belli.2158@gmail.com>
2025-05-19 20:06:23 +0200

    add: busy-waiting send with while (to fix)

--- Stencil2D/run.py
+++ Stencil2D/run.py
@@ -29,7 +29,7 @@ print(f"\nSimulation Info\n  rows: {M}\n  cols: {N}\n  iterations: {iterations}\
 # Construct A
 np.random.seed(0)
 A = (np.random.rand(N*M) * 10).astype(np.float32)
- 
+
 M_per_PE = M // h
 N_per_PE = N // w
 halo = 1
@@ -80,10 +80,11 @@ for _ in range(iterations):
   for i in range(M):
     for j in range(N):
       if(i-1 >= 0): y_aux[i*N+j] += y[(i-1)*N+j]
-      y_aux[i*N+j] += y[i*N+j]
       if(i+1 < M) : y_aux[i*N+j] += y[(i+1)*N+j]
       if(j-1 >= 0): y_aux[i*N+j] += y[i*N+j-1]
+      y_aux[i*N+j] -= 4.0*y[i*N+j]
       if(j+1 < N) : y_aux[i*N+j] += y[i*N+j+1]
+
 
   temp = y.copy()
   y = y_aux.copy()
@@ -93,6 +94,15 @@ y_expected = y.copy()
 
 print(f'{A.reshape(M,N)}\n')
 print(f'{y_expected.reshape(M,N)}\n')  
-print(f'{y_result.reshape(M,N)}\n')    
-np.testing.assert_allclose(y_result.ravel(), y_expected, atol=1e-5, rtol=1e-7)
+print(f'{y_result.reshape(M,N)}\n')  
+
+with open("wse_result.txt", "w+") as f:
+  for i in y_result.ravel():
+    print(f'{i}', file=f)  
+
+with open("py_result.txt", "w+") as f:
+  for i in y_expected:
+    print(f'{i}', file=f)  
+
+np.testing.assert_allclose(y_result.ravel(), y_expected, atol=0, rtol=0)
 print("SUCCESS!")
---
+++ Stencil2D/stencil.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int M = 8;  // rows
+  int N = 8;  // cols
+  int iterations = 10;
+
+  float* A = (float*) malloc(M*N *sizeof(float));
+  float* A_aux = (float*) calloc(M*N, sizeof(float));
+  
+  float x = 0;
+  for(int i = 0; i < M; i++){
+    for(int j = 0; j < N; j++){
+      A[i*N + j] = x;
+      x++;
+    }
+  }
+
+  for(int iter = 0; iter < iterations; iter++){
+    for(int i = 0; i < M; i++){
+      for(int j = 0; j < N; j++){
+        if(i-1 >= 0)  A_aux[i*N + j] += A[(i-1)*N + j];
+        if(i+1 < M)   A_aux[i*N + j] += A[(i+1)*N + j];
+        if(j-1 >= 0)  A_aux[i*N + j] += A[i*N + j-1];
+        A_aux[i*N + j] -= 4.0 * A[i*N + j];
+        if(j+1 < N)   A_aux[i*N + j] += A[i*N + j+1];
+      }
+    }
+
+    float* temp = A;
+    A = A_aux;
+    A_aux = temp;
+  }
+
+  FILE* fp = fopen("c_result.txt","w+");
+  for(int i = 0; i < M; i++){
+    for(int j = 0; j < N; j++){
+      printf("%f ", A[i*N +j]);
+      fprintf(fp, "%.1f \n", A[i*N +j]);
+    }
+    printf("\n");
+  }
+  fclose(fp);
+}
