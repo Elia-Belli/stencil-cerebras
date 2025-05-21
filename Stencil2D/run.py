61317923fb0b3eb690439d15d0d714c74ee079af
Elia-Belli <elia.belli.2158@gmail.com>
2025-05-21 16:01:21 +0200

    add: min/max cycles with <time> module

--- Stencil2D/run.py
+++ Stencil2D/run.py
@@ -3,6 +3,9 @@
 import argparse
 import json
 import numpy as np
+import math
+
+from utils.timestamp import float_to_hex, sub_ts
 
 from cerebras.sdk.runtime.sdkruntimepybind import SdkRuntime, MemcpyDataType, MemcpyOrder # type: ignore # pylint: disable=no-name-in-module
 
@@ -43,32 +46,49 @@ runner = SdkRuntime(args.name, cmaddr=args.cmaddr)
 
 # Get symbols for A on device
 A_symbol = runner.get_id('A')
-
+symbol_maxmin_time = runner.get_id("maxmin_time")
 # Load and run the program
 runner.load()
 runner.run()
 
-# Prepare A
+print("Copying A onto Device...")
 A_prepared = A.reshape(w, M_per_PE, h, N_per_PE).transpose(0, 2, 1, 3)
 A_prepared = np.pad(A_prepared, ((0, 0), (0, 0), (halo, halo), (halo, halo)), mode='constant', constant_values=0).ravel()
-# Copy A to device
+print("DONE!")
 runner.memcpy_h2d(A_symbol, A_prepared, 0, 0, w, h, elements_per_PE, streaming=False,
   order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
 
+print("Starting Computation...")
 runner.launch('compute', nonblock=False)
+print("DONE!")
 
+
+print("Copying back result...")
 # Copy y back from device
 runner.memcpy_d2h(y_result, A_symbol, 0, 0, w, h, elements_per_PE, streaming=False,
   order=MemcpyOrder.ROW_MAJOR, data_type=MemcpyDataType.MEMCPY_32BIT, nonblock=False)
 # Unpack y_result
 y_result = y_result.reshape(w, h, *tiled_shape)
 y_result = y_result[:,:, halo:-halo, halo:-halo].transpose(0, 2, 1, 3)
+print("DONE!")
+
+
+print("Copying back timestamps")
+data = np.zeros((w*h*3), dtype=np.uint32)
+runner.memcpy_d2h(data, symbol_maxmin_time, 0, 0, w, h, 3,
+  streaming=False, data_type=MemcpyDataType.MEMCPY_32BIT, order=MemcpyOrder.ROW_MAJOR, nonblock=False)
+maxmin_time_hwl = data.view(np.float32).reshape((h, w, 3))
+print("DONE!\n")
+
 
 # Stop the program
 runner.stop()
 
 
-# Ensure that the result matches our expectation
+############################
+##      Check Result      ##
+############################
+print("Verifying result\n")
 y = np.zeros(M*N, dtype=np.float32)
 temp = np.zeros(M*N, dtype=np.float32)
 y_aux = np.zeros(M*N, dtype=np.float32)
@@ -76,6 +96,7 @@ y_expected = np.zeros(M*N, dtype=np.float32)
 
 y = A.copy()
 
+# calculating host result
 for _ in range(iterations):
   for i in range(M):
     for j in range(N):
@@ -92,9 +113,9 @@ for _ in range(iterations):
 
 y_expected = y.copy()    
 
-print(f'{A.reshape(M,N)}\n')
-print(f'{y_expected.reshape(M,N)}\n')  
-print(f'{y_result.reshape(M,N)}\n')  
+print(f'Input:\n{A.reshape(M,N)}\n')
+#print(f'{y_expected.reshape(M,N)}\n')  
+print(f'Output:\n{y_result.reshape(M,N)}\n')  
 
 with open("./out/output/wse_result.txt", "w+") as f:
   for i in y_result.ravel():
@@ -105,4 +126,38 @@ with open("./out/output/py_result.txt", "w+") as f:
     print(f'{i}', file=f)  
 
 np.testing.assert_allclose(y_result.ravel(), y_expected, atol=0, rtol=0)
-print("SUCCESS!")
+print("SUCCESS!\n")
+
+
+###################
+##  Timestamps   ##
+###################
+tsc_tensor_d2h = np.zeros(6).astype(np.uint16)
+min_cycles = math.inf
+max_cycles = 0
+
+for w in range(w):
+  for h in range(h):
+    hex_t0 = int(float_to_hex(maxmin_time_hwl[(h, w, 0)]), base=16)
+    hex_t1 = int(float_to_hex(maxmin_time_hwl[(h, w, 1)]), base=16)
+    hex_t2 = int(float_to_hex(maxmin_time_hwl[(h, w, 2)]), base=16)
+    tsc_tensor_d2h[0] = hex_t0 & 0x0000ffff
+    tsc_tensor_d2h[1] = (hex_t0 >> 16) & 0x0000ffff
+    tsc_tensor_d2h[2] = hex_t1 & 0x0000ffff
+    tsc_tensor_d2h[3] = (hex_t1 >> 16) & 0x0000ffff
+    tsc_tensor_d2h[4] = hex_t2 & 0x0000ffff
+    tsc_tensor_d2h[5] = (hex_t2 >> 16) & 0x0000ffff
+
+    cycles = sub_ts(tsc_tensor_d2h)
+    if cycles < min_cycles:
+      min_cycles = cycles
+      min_w = w
+      min_h = h
+    if cycles > max_cycles:
+      max_cycles = cycles
+      max_w = w
+      max_h = h
+
+print("Cycle Counts:")
+print("Min cycles (", min_w, ", ", min_h, "): ", min_cycles)
+print("Max cycles (", max_w, ", ", max_h, "): ", max_cycles)
